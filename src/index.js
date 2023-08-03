c751667ade9cc3a51e6c1c093dd71823841eacb8
Stepanenko1993 <109295820+Stepanenko1993@users.noreply.github.com>
2023-08-03 23:06:14 +0300

    hw-8-fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { store, persistor } from 'redux/store';
+import App from 'components/App';
+import { HashRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import './styles/index.css';
+import Loader from 'components/Loader';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+    <HashRouter>
+        <PersistGate loading={<Loader />} persistor={persistor}>
+          <App />
+        </PersistGate>
+        </HashRouter>
+    </Provider>
+  </React.StrictMode>,
 );
+
+
